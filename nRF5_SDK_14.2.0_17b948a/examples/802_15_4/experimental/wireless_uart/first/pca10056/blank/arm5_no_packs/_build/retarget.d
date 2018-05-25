8a77e55a4c2348d403bd6a8a7f22bc902069a209
zhangmuyang <279401783@qq.com>
2018-05-25 10:17:12 +0800

    first  commit

---
+++ nRF5_SDK_14.2.0_17b948a/examples/802_15_4/experimental/wireless_uart/first/pca10056/blank/arm5_no_packs/_build/retarget.d
@@ -0,0 +1,5 @@
+.\_build\retarget.o: ..\..\..\..\retarget.c
+.\_build\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\_build\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
+.\_build\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\_build\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
